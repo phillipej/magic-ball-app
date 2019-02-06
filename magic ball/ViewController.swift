3a7a1d6ec664283702f12d35865b44a082e4c2f7
Phillipe Jórdan <phillipe.j@gmail.com>
2019-02-06 01:57:27 -0200

    Initial Commit

---
+++ magic ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic ball
+//
+//  Created by Phillipe Osorio on 06/02/19.
+//  Copyright © 2019 Phillipe Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
