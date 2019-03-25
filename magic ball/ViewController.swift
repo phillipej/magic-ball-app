ca5c2d147148c836800907533242a8327cd38c5b
Phillipe Jórdan <phillipe.j@gmail.com>
2019-03-25 17:52:11 -0300

    testing

--- magic ball/ViewController.swift
+++ magic ball/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let respostas = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var numeroAleatorio: Int = 0
+    
+    
 
+    @IBOutlet weak var revelacao: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        changeAnswer()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        changeAnswer()
+    }
+    
+    @IBAction func butao(_ sender: UIButton) {
+        changeAnswer()
+    }
+    
+    
+    func changeAnswer (){
+        
+        numeroAleatorio = Int.random(in: (0 ... 4))
+        
+        revelacao.image = UIImage (named: respostas[numeroAleatorio])
+        
+    }
 
 }
 
